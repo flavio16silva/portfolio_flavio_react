b8e546ea7dffb758422173b82174e6c303f09634
Flávio Silva <fal_silva@hotmail.com>
2026-01-21 15:31:45 -0300

    ✅ Migração do projeto html, css e js para react - montagem do esqueleto

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,41 @@
-const App = () => <h1>Boilerplate Vite + React</h1>
+const App = () => {
+  return (
+    <>
+      {/* NAVBAR */}
+      <nav>
+        <h1>Flávio</h1>
+      </nav>
+
+      {/* HEADER */}
+      <header>
+        <h2>Olá, eu sou Flávio</h2>
+        <p>Desenvolvedor Frontend</p>
+      </header>
+
+      {/* SOBRE */}
+      <section id="sobre">
+        <h2>Sobre</h2>
+        <p>
+          Texto sobre mim.
+        </p>
+      </section>
+
+      {/* HABILIDADES */}
+      <section id="habilidades">
+        <h2>Habilidades</h2>
+      </section>
+
+      {/* PROJETOS */}
+      <section id="projetos">
+        <h2>Projetos</h2>
+      </section>
+
+      {/* FOOTER */}
+      <footer>
+        <p>© 2026 - Flávio Silva</p>
+      </footer>
+    </>
+  )
+}
 
 export { App }
