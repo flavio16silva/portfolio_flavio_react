7a4a6c4e4001b5fe2cd8509123352b6e46a3fcb4
Flávio Silva <fal_silva@hotmail.com>
2026-01-21 18:49:36 -0300

    ✅  feat: adiciona estrutura de Habilidades

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,128 @@ const App = () => {
       </section>
 
       {/* HABILIDADES */}
+      <section id="skills" className="skills">
+        <div className="skills__content max-width">
+          <h2 className="tertiary-title">Minha caixa de ferramentas</h2>
+          <p className="description">
+            Descubra as habilidades e ferramentas que busco aprender, aprimorar e que me permitem
+            criar soluções criativas e funcionais para todos.
+          </p>
+          <ul>
+            <li className="skills__item">
+              <div className="image-container">
+                <img src="assets/notebook.png" alt="Desenvolvimento Web" />
+              </div>
+              <h3>Desenvolvimento Web</h3>
+              <p>
+                No desenvolvimento Front End com foco em tecnologias
+                modernas, executando trilhas, desafios e projetos que aprimorem o raciocínio, criatividade e prática.
+              </p>
+            </li>
 
+            <li className="skills__item">
+              <div className="image-container">
+                <img src="assets/files.png" alt="Gerenciamentos de versões" />
+              </div>
+              <h3>Gerenciamentos de versões</h3>
+              <p>
+                Estudo e experiência em ferramentas de gerenciamento de versão para
+                controle de projetos com maior uso de GitHub, com manuseio e testes em GitLab e Bitbucket.
+              </p>
+            </li>
+          </ul>
+
+          <p className="linguagens">Abaixo algumas das linguagens que estudo, prático, crio e executo projetos.</p>
+
+          {/* LINHA DE LINGUAGENS */}
+          <div className="conteudo">
+            {/* Uma Linha de Habilidades*/}
+            <div className="linha" id="caixaskills">
+              <div className="bar">
+                <div className="info">
+                  <img src="assets/html1.png" />
+                  <span>HTML</span>
+                  <h3>70%</h3>
+                </div>
+              </div>
+
+              <div className="bar">
+                <div className="info">
+                  <img src="assets/css1.png" />
+                  <span>CSS3</span>
+                  <h3>50%</h3>
+                </div>
+              </div>
+
+              <div className="bar">
+                <div className="info">
+                  <img src="assets/js.png" />
+                  <span>JavaScript</span>
+                  <h3>25%</h3>
+                </div>
+              </div>
+
+              {/*Teste*/}
+              <div className="bar">
+                <div className="info">
+                  <img src="assets/java1.png" />
+                  <span>JAVA</span>
+                  <h3>15%</h3>
+                </div>
+              </div>
+
+              <div className="bar">
+                <div className="info">
+                  <img src="assets/python.png" />
+                  <span>PYTHON</span>
+                  <h3>10%</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/*SLIDER - CARDS*/}
+          <section id="port">
+            <div className="container">
+              <h2 className="secondary-title">Rotina de Estudos</h2>
+              <p className="linguagens">Considerações de um profissional apaixonado por tecnologia. </p>
+              <input type="radio" name="slider" id="item-1" checked />
+              <input type="radio" name="slider" id="item-2" />
+              <input type="radio" name="slider" id="item-3" />
+
+              {/*Seção dos Cards*/}
+              <div className="cards">
+                <label className="card" for="item-1" id="panf-1">
+                  <img className="foto" src="assets/cards13.png" alt="" />
+                  <h1>Web</h1>
+                  <p>
+                    Nesse campo de trabalho crescente da <strong>Web</strong> busco informações, projetos e criações que podem ser aprimoradas. São rotinas e prazos de entrega que geram melhorias no dinamismo e aceleração das práticas diárias.
+                  </p>
+                  <a href="#footer"><button>contrate-me</button></a>
+                </label>
+
+                <label className="card" for="item-2" id="panf-2">
+                  <img src="assets/cards9.jpg" alt="" />
+                  <h1>Responsivo</h1>
+                  <p>
+                    Um campo de estudos e criação de projetos, a <strong>Responsividade</strong> tem proporcionado boas práticas com mobile first gerando desafios e melhorias continuas. As ferramentas de Grid e Flexbox tem amparado e norteado a compreensão da adaptabilidade dos dispositivos.
+                  </p>
+                  <a href="#footer"><button>contrate-me</button></a>
+                </label>
+
+                <label className="card" for="item-3" id="panf-3">
+                  <img src="assets/cards7.jpg" alt="" />
+                  <h1>Estudos</h1>
+                  <p>
+                    Explorando projetos com maior grau de complexidade e buscando metas alcançaveis  nos <strong>estudos</strong>, isso tem gerado desafios e bons resultados. São anotações, planejamentos, escolhas de layout e aprimoramento das técnicas de uso das tecnologias.
+                  </p>
+                  <a href="#footer"><button>contrate-me</button></a>
+                </label>
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
 
       {/* PROJETOS */}
 
