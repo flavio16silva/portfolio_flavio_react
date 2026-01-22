c0d2f4d204f8f4c5d47818edca3c01f53d1df6b6
Flávio Silva <fal_silva@hotmail.com>
2026-01-21 22:01:53 -0300

    ✅  feat: adiciona estrutura do footer

--- src/App.jsx
+++ src/App.jsx
@@ -434,8 +434,73 @@ const App = () => {
       </section>
 
       {/* FOOTER */}
-      <footer>
-        <p>© 2026 - Flávio Silva</p>
+      <footer id="footer">
+        <div className="footer__content">
+          <h2 className="tertiary-title mb-s">Vamos conversar?</h2>
+          <p className="conversar mb-m">
+            Ficarei feliz em saber mais sobre seus projetos e como podemos
+            trabalhar juntos para torná-los realidade.
+          </p>
+          <section className="rodape">
+            <div className="box-container">
+              <div className="caixa">
+                <h3>Portifólio Flávio Silva</h3>
+                <p>
+                  Obrigado por Visitar meu Portifólio. <br />
+                  Visite-me nas redes sociais. <br />
+                  <br />
+                </p>
+              </div>
+
+              <div className="caixa">
+                <h3>Links Rápidos</h3>
+                <a href="#header"
+                ><i className="fas fa-chevron-circle-right"></i> Home</a
+                >
+                <a href="#about"
+                ><i className="fas fa-chevron-circle-right"></i> Sobre</a
+                >
+                <a href="#skills"
+                ><i className="fas fa-chevron-circle-right"></i> Habilidades</a
+                >
+                <a href="#projects"
+                ><i className="fas fa-chevron-circle-right"></i> Projetos</a
+                >
+              </div>
+
+              <div className="caixa">
+                <h3>Informações para Contato</h3>
+                <p><i className="fas fa-phone"></i>+55 (71) 99836-2020</p>
+                <p><i className="fas fa-envelope"></i>fal_silva@hotmail.com</p>
+                <div className="share">
+                  {/* <a
+                  href="https://www.linkedin.com/in/fl%C3%A1vio-silva-7080b848/"
+                  className="fab fa-linkedin"
+                  aria-label="LinkedIn"
+                  target="_blank"
+                ></a>
+                <a
+                  href="https://github.com/flavio16silva"
+                  className="fab fa-github"
+                  aria-label="GitHub"
+                  target="_blank"
+                ></a>
+                <a 
+                href="#" 
+                className="fas fa-envelope" 
+                aria-label="Mail" 
+                target="_blank" 
+                ></a> */}
+                </div>
+              </div>
+            </div>
+          </section>
+
+          {/* <!-- Direitos Reservados --> */}
+          <p className="footer__copyright">
+            © 2023 Flávio Silva. Todos os direitos reservados.
+          </p>
+        </div>
       </footer>
     </>
   )
