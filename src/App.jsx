0e5a6901074210ebbc7a39b0efc116d8a88cf9ea
Flávio Silva <fal_silva@hotmail.com>
2026-01-22 15:20:00 -0300

    ✅ Componente MediaSocial criado

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,30 @@ const Header = () =>
         </div>
       </nav>
     </header>
-  </>  
+  </>
+
+const MediaSocial = () => (
+  <section>
+    <a
+      href="https://www.linkedin.com/in/fl%C3%A1vio-silva-7080b848/"
+      className="fab fa-linkedin"
+      aria-label="LinkedIn"
+      target="_blank" rel="noopener noreferrer"
+    ></a>
+    <a
+      href="https://github.com/flavio16silva"
+      className="fab fa-github"
+      aria-label="GitHub"
+      target="_blank" rel="noopener noreferrer"
+    ></a>
+    <a
+      href="mailto:fal_silva@hotmail.com"
+      className="fas fa-envelope"
+      aria-label="Email"
+      target="_blank" rel="noopener noreferrer"
+    ></a>
+  </section>
+)
 
 const Inicio = () =>
   <>
