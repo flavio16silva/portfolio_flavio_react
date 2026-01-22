c19d4157a25e06c87f041ba570df4c8d4e2f419f
Flávio Silva <fal_silva@hotmail.com>
2026-01-22 15:29:21 -0300

    ✅ Componente About - refatorado e refeito

--- src/App.jsx
+++ src/App.jsx
@@ -63,9 +63,18 @@ const About = () =>
       <h1>Olá! Sou Flávio Silva Desenvolvedor Front End</h1>
       <p className="texto">Hoje, <span className="escrever-texto"></span></p>
       <p>
-        Desenvolvedor Front-End em crescimento através de experiências com criação de projetos escaláveis e responsivos. Sou atraído por criar soluções tecnológicas e pesquisas que me impulsionam na direção dos projetos de Back-End, resolvendo problemas reais e agregando valor aos usuários. A cada desafio, meu desejo de explorar o mundo do desenvolvimento de Back-End cresce, e estou empolgado para embarcar nessa nova etapa da minha carreira.
+        Olá, sou Flávio Silva, um entusiasta de desenvolvimento Front-End em busca constante de conhecimento e aprendizado nas mais recentes tecnologias.
+        Embora minha experiência em desenvolvimento web seja recente, tenho dedicado meu tempo a trabalhar com diversas tecnologias e plataformas,
+        com o objetivo de criar soluções que atendam de forma eficaz às necessidades dos clientes.
+        Desenvolvedor Front-End em crescimento através de experiências com criação de projetos escaláveis e responsivos.
+        Sou atraído por criar soluções tecnológicas e pesquisas que me impulsionam na direção dos projetos de Back-End,
+        resolvendo problemas reais e agregando valor aos usuários. A cada desafio, meu desejo de explorar o mundo do desenvolvimento de Back-End cresce,
+        e estou empolgado para embarcar nessa nova etapa da minha carreira.
+        Desafios complexos são minha motivação, e estou sempre em busca de soluções inovadoras e criativas para superá-los.
+        A cada dia, reservo tempo para aprofundar meu entendimento e aprimorar minhas habilidades nas tecnologias atuais.
+        Minha paixão pela aprendizagem constante é o motor que impulsiona meu crescimento profissional.
+        Estou ansioso para enfrentar novos desafios e contribuir para projetos empolgantes.
       </p>
-      <a href="#about" className="btn btn-primary">Saiba Mais!<i className="fas fa-arrow-circle-down"></i></a>
     </div>
 
     <div className="header__right">
@@ -74,32 +83,6 @@ const About = () =>
         <img src="/images/abstract.svg" alt="background" className="background" />
       </div>
     </div>
-    <section id="about" className="about max-width">
-      <div className="about__left">
-        <h2 className="secondary-title">Além do código: quem sou eu?</h2>
-        <p>
-          Que tal se conectar comigo nas redes sociais abaixo e saber mais sobre
-          meu trabalho?
-        </p>
-        <ul>
-          <li>
-            <a href="https://www.linkedin.com/in/fl%C3%A1vio-silva-7080b848/" target="_blank" rel="noopener noreferrer"><img src="/images/linkedin.svg" alt="Linkedin" /></a>
-          </li>
-        </ul>
-      </div>
-      <div className="about__right">
-        <h3 className="tertiary-title mb-m">
-          Desenvolvedor Front End descobrindo os caminhos da tecnologia e as soluções criativas.
-        </h3>
-        <p>
-          Olá, sou Flávio Silva, um entusiasta de desenvolvimento Front-End em busca constante de conhecimento e aprendizado nas mais recentes tecnologias. Embora minha experiência em desenvolvimento web seja recente, tenho dedicado meu tempo a trabalhar com diversas tecnologias e plataformas, com o objetivo de criar soluções que atendam de forma eficaz às necessidades dos clientes.
-        </p>
-        <p>
-          Desafios complexos são minha motivação, e estou sempre em busca de soluções inovadoras e criativas para superá-los. A cada dia, reservo tempo para aprofundar meu entendimento e aprimorar minhas habilidades nas tecnologias atuais. Minha paixão pela aprendizagem constante é o motor que impulsiona meu crescimento profissional. Estou ansioso para enfrentar novos desafios e contribuir para projetos empolgantes.
-
-        </p>
-      </div>
-    </section>
   </>
 
 const Habilidades = () =>
