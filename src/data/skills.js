6be136e97f52b133147c9dadd56bec4909cb7431
Flávio Silva <fal_silva@hotmail.com>
2026-01-30 14:12:36 -0300

    ✅ Criação do array de skills.js

---
+++ src/data/skills.js
@@ -0,0 +1,31 @@
+import {
+  SiHtml5,
+  SiCss3,
+  SiReact,
+  SiJavascript,
+  SiBootstrap,
+  SiTailwindcss,
+  SiLinux,
+  SiGit,
+  SiGithub,
+  SiPostgresql
+} from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+
+export const skills = [
+  { name: "", icon: SiHtml5 },
+  { name: "CSS", icon: SiCss3 },
+  { name: "Javascript", icon: SiJavascript },
+  { name: "Bootstrap", icon: SiBootstrap },
+  { name: "React", icon: SiReact },
+  { name: "Tailwind", icon: SiTailwindcss },
+  { name: "Linux", icon: SiLinux },
+  { name: "Git", icon: SiGit },
+  { name: "Github", icon: SiGithub },
+  { name: "Mysql", icon: GrMysql },
+  { name: "Postgresql", icon: SiPostgresql },
+]
+
+
+
+
