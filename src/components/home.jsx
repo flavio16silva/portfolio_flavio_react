e899acab8f69ff89ae6b5df1c4095e3803adab45
Flávio Silva <fal_silva@hotmail.com>
2026-02-05 07:09:42 -0300

    ✅ Home - Exclusão de código - mais limpo

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,95 +1,40 @@
-const splitName = (fullName) => {
-  const parts = fullName.split(" ")
-
-  return {
-    firstLine: parts.slice(0, 2).join(" "),
-    secondLine: parts.slice(2).join(" ")
-  }
-}
-
-const splitLetters = (text) => text.split("")
-
 const Home = () => {
-  const name = splitName("FLÁVIO LEITE DA SILVA")
-  const containerClass = "max-w-7xl mx-auto px-12 md:px-24 lg:px-32"
-
+  const containerClass = "max-w-7xl mx-auto px-4 sm:px-8 md:px-12 lg:px-32"
   return (
     <header
-      id="header" 
-      className="relative min-h-screen bg-cover bg-top bg-[50_30%]" 
-      style={{ backgroundImage: "url('/images/bg-home1.jpg')" }} >
+      id="header"
+      className="relative min-h-screen bg-cover bg-top bg-[50_30%]"
+      style={{ backgroundImage: "url('/images/bg-home2.jpg')" }} >
 
       {/* HERO */}
-      <div className={`flex flex-col justify-center min-h-screen pt-24 ${containerClass}`} >
-        <h1 className="mb-10 leading-none w-full flex flex-col items-center text-center sm:items-start sm:text-left">
-          {/* PRIMEIRO NOME */}
-          <span className={`block text-3xl sm:text-4x1 md:text-6xl lg:text-7xl font-bold
-           text-emerald-400 duration-700 ease-out 
-           `}
-          > {name.firstLine} </span>
+      <div className={`flex flex-col lg:flex-row items-center justify-center lg:justify-between min-h-screen ${containerClass}`} >
+        {/* TEXTO */}
+        <div className="lg:w-3/5 flex flex-col justify-center mb-6 sm:mb-8 lg:mb-0">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold text-emerald-400 ease-out text-center lg:text-left"> FLÁVIO SILVA </h1>
 
-          {/* SEGUNDO NOME */}
-          <span
-            className={`mt-4 text-base md:text-xl lg:text-2xl font-semibold uppercase text-emerald-400 
-            tracking-[0.7em] md:tracking-[2.0em] lg:tracking-[2.0em] pl-1 sm:pl-2 md:pl-2 lg:pl-2 duration-700 ease-out
-            sm:-translate-x-[0.35em]
-            md:-translate-x-[0.2em]
-            lg:-translate-x-[0.2em]
-            `}
-          >
-            <>
-              <span className="inline-flex translate-x-[0.22em] sm:translate-x-0">
-                {splitLetters("DA").map((letter, index) => (
-                  <span key={`da-${index}`}>{letter}</span>
-                ))}
-              </span>
+          {/* SUBTÍTULO */}
+          <div className="flex flex-wrap items-center gap-4 text-emerald-400 uppercase tracking-wider justify-center lg:justify-start mt-4 sm:mt-6 ">
+            <span className=" block w-full text-center lg:w-auto lg:inline-flex ">
+              Desenvolvedor Front-end</span>
+            <span className=" block w-full text-center lg:w-auto lg:inline-flex ">
+              Estudante de Desenvolvimento Web Full Stack</span>
+          </div>
+        </div>
 
-              <span className="mx-6 opacity-50" />
-
-              <span className="inline-flex translate-x-[0.5em] sm:translate-x-0 md:pl-0 pl-[0.3em]">
-                {splitLetters("SILVA").map((letter, index) => (
-                  <span key={`silva-${index}`}>{letter}</span>
-                ))}
-              </span>
-            </>
-          </span>
-        </h1>
-
-
-        {/* SUBTÍTULO */}
-        <div className="
-        flex 
-        flex-wrap 
-        items-center 
-        gap-4 
-        text-emerald-400 
-        uppercase 
-        text-center 
-        md:text-center 
-        tracking-wider
-        ">
-          <span className="
-              block
-              w-full
-              text-center
-              sm:w-auto
-              sm:inline-flex
-              ">
-            Desenvolvedor Front-end</span>
-          <span className="hidden sm:inline opacity-70 font-extrabold ">|</span>
-          <span className="
-                block
-                w-full
-                text-center
-                sm:w-auto
-                sm:inline-flex
-                ">
-            Estudante de Desenvolvimento Web Full Stack</span>
+        {/* FOTO */}
+        <div className="lg:w-2/5 flex justify-center lg:justify-end
+        mt-6 sm:mt-8 lg:mt-0">
+          <img
+            src="/images/fotoEu.jpg"
+            alt="Flavio Silva"
+            className="w-48 sm:w-56 md:w-64 lg:w-80 xl:w-96 
+                     h-auto object-cover rounded-xl shadow-[0_0_30px_10px_rgba(255,255,255,0.2)]
+                     transform hover:scale-105 transition duration-300"
+            style={{ maxHeight: '420px', objectPosition: 'center top' }} />
         </div>
       </div>
     </header >
   )
 }
-
 
 export { Home }
