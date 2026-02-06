20613bb5fc3630451ebe4aeaaf833623533c547e
Flávio Silva <fal_silva@hotmail.com>
2026-02-06 10:24:01 -0300

    ✅ Criação do array para os projetos

---
+++ src/data/projects.js
@@ -0,0 +1,66 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Salmo 91",
+    image: "/images/Salmo91.png",
+    repoLink: "#",
+    infoTitle: "Efeito Parallax como projeto pessoal.",
+    description: "Meu primeiro projeto envolveu a manipulação de imagens, texto e efeitos de parallax. Uma oportunidade de aprender sobre responsividade, media queries e o uso de unidades relativas.",
+    tech: ["HTML", "CSS", "Javascript"]
+  },
+  {
+    id: 2,
+    title: "Calculadora de Gorjetas",
+    image: "/images/calculadoraGorjeta.png",
+    repoLink: "#",
+    infoTitle: "Calculadora de Gorjeta",
+    description: "Projeto em JavaScript com laços e manipulação do DOM e aplicação do CSS puro.",
+    tech: ["HTML", "CSS", "Javascript"]
+  },
+  {
+    id: 3,
+    title: "Conceitos de mobile-first",
+    image: "/images/telaLogin.png",
+    repoLink: "#",
+    infoTitle: "Tela de Login Mobile com Redes Sociais",
+    description: "Tela de login mobile desenvolvida com responsividade pelas media queries, ícones de mídias sociais ativos e formulário de login. Aplicados técnicas de responsividade em diferentes dispositivos.",
+    tech: ["HTML", "CSS"]
+  },
+  {
+    id: 4,
+    title: "Formulário de Cadastro",
+    image: "/images/FormCadastro.png",
+    repoLink: "#",
+    infoTitle: "Formulário de Cadastro",
+    description: "Formulário criado com HTML e CSS para cadastro de informações gerais com tags semânticas, conceitos iniciais de Front End.",
+    tech: ["HTML", "CSS"]
+  },
+  {
+    id: 5,
+    title: "Horário do Dia",
+    image: "/images/HoradoDia.png",
+    repoLink: "#",
+    infoTitle: "Horário do Dia",
+    description: "Projeto com HTML, CSS e JavaScript trabalhando interação com funções, DOM e o sistema operacional do Windows.",
+    tech: ["HTML", "CSS", "Javascript"]
+  },
+  {
+    id: 6,
+    title: "Feira de Frutas",
+    image: "/images/FeiraFrutas.png",
+    repoLink: "#",
+    infoTitle: "Cards com Feira de Frutas",
+    description: "Projeto focado em inputs do HTML, responsividade do CSS e tratativa de lógica com valores de inputs, atributos, template strings e manipulando o DOM pelo JavaScript.",
+    tech: ["HTML", "CSS", "Javascript"]
+  },
+  {
+    id: 7,
+    title: "Controle Financeiro",
+    image: "/images/controleFinanceiro.jpg",
+    repoLink: "#",
+    infoTitle: "Controle Financeiro",
+    description: "Um Controle Financeiro, onde adiciona as transações no DOM, executa o preenchimento das informações do estado da aplicação quando a pagina for carregada, atualiza o valor das transações na tela (Saldo, Receitas e Despesas), armazenamento e adição das Transações Realizadas no Local Storage, remoção de Transações e execução dos eventos no formulário.",
+    tech: ["HTML", "CSS", "Javascript"]
+  }
+]
+
