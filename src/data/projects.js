bbf2bce13fb9998b400ffdc4e5a6440c0c49ca59
Flávio Silva <fal_silva@hotmail.com>
2026-02-06 11:22:38 -0300

    ✅ Inserção dos endereços dos repo dos projetos

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     id: 1,
     title: "Salmo 91",
     image: "/images/Salmo91.png",
-    repoLink: "#",
+    repoLink: "https://github.com/flavio16silva/Salmo91",
     infoTitle: "Efeito Parallax como projeto pessoal.",
     description: "Meu primeiro projeto envolveu a manipulação de imagens, texto e efeitos de parallax. Uma oportunidade de aprender sobre responsividade, media queries e o uso de unidades relativas.",
     tech: ["HTML", "CSS", "Javascript"]
@@ -12,7 +12,7 @@ export const projects = [
     id: 2,
     title: "Calculadora de Gorjetas",
     image: "/images/calculadoraGorjeta.png",
-    repoLink: "#",
+    repoLink: "https://github.com/flavio16silva/CalculadoraGorjeta",
     infoTitle: "Calculadora de Gorjeta",
     description: "Projeto em JavaScript com laços e manipulação do DOM e aplicação do CSS puro.",
     tech: ["HTML", "CSS", "Javascript"]
@@ -21,7 +21,7 @@ export const projects = [
     id: 3,
     title: "Conceitos de mobile-first",
     image: "/images/telaLogin.png",
-    repoLink: "#",
+    repoLink: "https://github.com/flavio16silva/Projeto_Login",
     infoTitle: "Tela de Login Mobile com Redes Sociais",
     description: "Tela de login mobile desenvolvida com responsividade pelas media queries, ícones de mídias sociais ativos e formulário de login. Aplicados técnicas de responsividade em diferentes dispositivos.",
     tech: ["HTML", "CSS"]
@@ -30,7 +30,7 @@ export const projects = [
     id: 4,
     title: "Formulário de Cadastro",
     image: "/images/FormCadastro.png",
-    repoLink: "#",
+    repoLink: "https://github.com/flavio16silva/Devs",
     infoTitle: "Formulário de Cadastro",
     description: "Formulário criado com HTML e CSS para cadastro de informações gerais com tags semânticas, conceitos iniciais de Front End.",
     tech: ["HTML", "CSS"]
@@ -39,7 +39,7 @@ export const projects = [
     id: 5,
     title: "Horário do Dia",
     image: "/images/HoradoDia.png",
-    repoLink: "#",
+    repoLink: "https://github.com/flavio16silva/HoradoDia",
     infoTitle: "Horário do Dia",
     description: "Projeto com HTML, CSS e JavaScript trabalhando interação com funções, DOM e o sistema operacional do Windows.",
     tech: ["HTML", "CSS", "Javascript"]
@@ -48,7 +48,7 @@ export const projects = [
     id: 6,
     title: "Feira de Frutas",
     image: "/images/FeiraFrutas.png",
-    repoLink: "#",
+    repoLink: "https://github.com/flavio16silva/FeiraFrutas",
     infoTitle: "Cards com Feira de Frutas",
     description: "Projeto focado em inputs do HTML, responsividade do CSS e tratativa de lógica com valores de inputs, atributos, template strings e manipulando o DOM pelo JavaScript.",
     tech: ["HTML", "CSS", "Javascript"]
@@ -57,7 +57,7 @@ export const projects = [
     id: 7,
     title: "Controle Financeiro",
     image: "/images/controleFinanceiro.jpg",
-    repoLink: "#",
+    repoLink: "https://github.com/flavio16silva/Controle-Financeiro",
     infoTitle: "Controle Financeiro",
     description: "Um Controle Financeiro, onde adiciona as transações no DOM, executa o preenchimento das informações do estado da aplicação quando a pagina for carregada, atualiza o valor das transações na tela (Saldo, Receitas e Despesas), armazenamento e adição das Transações Realizadas no Local Storage, remoção de Transações e execução dos eventos no formulário.",
     tech: ["HTML", "CSS", "Javascript"]
