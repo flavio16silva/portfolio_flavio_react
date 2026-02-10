8936d1b952094df8f3987c52880d36d5853783af
Flávio Silva <fal_silva@hotmail.com>
2026-02-10 06:30:35 -0300

    ✅ Redes sociais mais usadas

---
+++ src/data/social.js
@@ -0,0 +1,21 @@
+import { MdOutlineMailOutline } from "react-icons/md"
+import { SiLinkedin, SiGithub } from "react-icons/si"
+
+export const socialIcon = [
+  {
+    name: "Github",
+    icon: SiGithub,
+    url: "https://github.com/flavio16silva"
+  },
+  {
+    name: "Linkedin",
+    icon: SiLinkedin,
+    url: "www.linkedin.com/in/flavio-leite-silva"
+  },
+  {
+    name: "Email",
+    icon: MdOutlineMailOutline,
+    url: "mailto:fal_silva@hotmail.com"
+  }
+]
+
