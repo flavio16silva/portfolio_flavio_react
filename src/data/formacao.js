550b3525f53d773e5e29aa218971b32ca5999721
Flávio Silva <fal_silva@hotmail.com>
2026-02-19 15:19:30 -0300

    Formação Page -> criação do array de faculdades

---
+++ src/data/formacao.js
@@ -0,0 +1,21 @@
+export const formacao = [
+  {
+    id: 1,
+    curso: "Administração com ênfase em Análise de Sistemas",
+    local: "Fundação Visconde de Cairu",
+    ano: "2002 - 2006"
+  },
+  {
+    id: 2,
+    curso: "Pós em Redes de Computadores e Telecomunicações",
+    local: "UNIFACS - Universidade Salvador",
+    ano: "2008 - 2010",
+  },
+  {
+    id: 3,
+    curso: "MBA em Gestão de Projetos",
+    local: "UNIFACS - Universidade Salvador",
+    ano: "2010 - 2012",
+  }
+]
+
